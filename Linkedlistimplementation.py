3521426da6e78bc5eba00f21e6789c3ae4a756fd
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-24 14:56:03 +0530

    now we traverse our linked list

--- Linkedlistimplementation.py
+++ Linkedlistimplementation.py
@@ -8,9 +8,12 @@ class Linkedlist():
     def __init__(self):
         self.headval=None
 
+
 list1=Linkedlist()
 list1.headval=Node("Mon")
 e2=Node("tue")
 e3=Node("wed")
 list1.headval.nextval=e2
 e2.nextval=e3
+
+
